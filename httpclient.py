216b1b67c08ab20ef182a4f80b781fba652dffae
ZhangBaoCai <talent518@yeah.net>
2017-08-24 16:01:38 +0800

    为httpclient添加命令行参数选项设置。

--- httpclient.py
+++ httpclient.py
@@ -5,6 +5,8 @@ import json
 import urllib2
 import base64
 import os
+import time
+import argparse
 
 def request(route, post):
     req = urllib2.Request('http://127.0.0.1:8000' + route)
@@ -48,15 +50,34 @@ def writefile(file, body):
     return ret
 
 if __name__ == '__main__':
-    response, status = request('/train', {'trains': 20, 'image': base64.encodestring(readfile('/home/VOCdevkit/JPEGImages/2007_003169.jpg')), 'label': base64.encodestring(readfile('/home/VOCdevkit/SegmentationClassAug/2007_003169.png'))})
+    parser = argparse.ArgumentParser(description="Deeplab Resnet Http Client.")
+    parser.add_argument("--host", type=str, default='127.0.0.1', help="connect to host(default: 127.0.0.1)")
+    parser.add_argument("--port", type=int, default=8000, help="connect to port(default: 8000)")
+    parser.add_argument("--trains", type=int, default=10, help="training steps(default: 10)")
+    parser.add_argument("--train-image", type=str, help="training image")
+    parser.add_argument("--train-label", type=str, help="training label")
+    parser.add_argument("--test-image", type=str, help="test label(default: --train-image)")
+    parser.add_argument("--test-label", type=str, help="test label(default: --test-image + suffix -mask.png)")
+    args = parser.parse_args()
+    if args.train_image is None:
+        parser.error('Not Found arguments --train-image')
+    if args.train_label is None:
+        parser.error('Not Found arguments --train-label')
+
+    response, status = request('/train', {'trains': args.trains, 'image': base64.encodestring(readfile(args.train_image)), 'label': base64.encodestring(readfile(args.train_label))})
 
     print '/train', status, response if not status else base64.decodestring(response)
 
-    response, status = request('/test', {'image': base64.encodestring(readfile('/home/VOCdevkit/JPEGImages/2007_003169.jpg'))})
+    if args.test_image is None:
+        args.test_image = args.train_image
+
+    if args.test_label is None:
+        args.test_label = args.test_image + '-mask-%d.png' % int(time.time())
+
+    response, status = request('/test', {'image': base64.encodestring(readfile(args.test_image))})
 
     if status:
-        file = './2007_003169.png'
-        writefile(file, base64.decodestring(response))
-        print '/test Save to ' + file
+        writefile(args.test_label, base64.decodestring(response))
+        print '/test Save to ' + args.test_label
     else:
         print '/test', response
